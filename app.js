f604cc9742ddd61775a49e7e686e2e23891cf783
PaulaAraya <paula.arayawilliams@gmail.com>
2018-05-28 10:30:27 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,9 @@
+//crear prompt para consultar edad
+//crear variables para edad 
+// If y Else para los resultados segun edad 
+
+window.onload = function() {
+    let age = prompt("¿cuantos años tienes?");
+     document.getElementById("age").innerText = age;
+}
+
